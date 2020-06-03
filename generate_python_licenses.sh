505031f61c373b58bf6740aeb09a41642dc47878
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-03 14:51:42 +0530

    Update generate_python_licenses.sh

--- generate_python_licenses.sh
+++ generate_python_licenses.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-licenses_file="$1"
 set -ex
 pip-licenses --with-system --with-authors --with-urls --format=csv --output-file=python_license.csv
-sed -i 's/"//g' "${licenses_file}"
+sed -i 's/"//g' python_license.csv
